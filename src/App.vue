b319a6b39a7a6fe0ac9ad9f0c97eccc2600d076f
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-15 15:45:21 +0200

    [+] Le formulaire apparaît maintenant seulement quand l'utilisateur clique sur 'ajouter une société'

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,17 @@
       </div>
       <div class="order">
         <button id="edit" :title="message['edit']" class="hover-item btn btn-cancel" @click="doEdit(true)">Éditer ficher ARCOLE</button>
-        <button id="retour" :title="message['retour']" class="hover-item" v-if="editing" @click="doEdit(false)">Retour</button>
+        <button id="retour" :title="message['retour']" class="hover-item" v-if="editing" @click="doEdit(false);
+        societe = !societe">Retour</button>
       </div>
     </header>
   </div>
   <template v-if="editing">
-    <button class="hover-item btn btn-cancel" @click="EditSociete">Ajouter une société</button>
+    <button class="hover-item btn btn-cancel" @click="societe = !societe">Ajouter une société</button>
     <button class="hover-item btn btn-cancel">Ajouter un établissement</button>
     <button class="hover-item btn btn-cancel">Ajouter une restaurant</button>
   </template>
-  <SocieteForm v-if="editing"/>
+  <SocieteForm v-if="societe"/>
   <div id="app2"></div>
   <footer> <a href="https://www.linkedin.com/in/alo%C3%AFs-brengard/" target="_blank">Author: Aloïs BRENGARD</a></footer>
 </template>
@@ -32,6 +33,7 @@ export default defineComponent({
   components: {SocieteForm},
   data() {
     return {
+      societe: false,
       editing: false,
       message: {
         'edit': 'Cliquer sur moi pour commencer à éditer le fichier.',
@@ -49,38 +51,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<!--
-setup() {
-    let SocietyClicked = [false, false, false];
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const json = require('../../json_file/arcole.json');
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const _ = require('@/functions/buttons');
-    const order = ref<OrderTerms>('society');
-    const HandleClick = (term: OrderTerms) => {
-      order.value = term;
-
-      if (order.value === 'society' && !SocietyClicked[0]) {
-        let app = document.querySelector('#app');
-        if (app == null) return;
-        let b = _.createButtons(json)
-
-        const pElement = document.createElement('div');
-        pElement.innerHTML = b
-        app.appendChild(pElement);
-        SocietyClicked[0] = true;
-      }
-      if (order.value === 'institution' && !SocietyClicked[1]) {
-         document.querySelectorAll('button')[1].disabled = true;
-        SocietyClicked[1] = true;
-      }
-      if (order.value === 'restaurant' && !SocietyClicked[2]) {
-        document.querySelectorAll('button')[2].disabled = true;
-        SocietyClicked[2] = true;
-      }
-    }
-    return {HandleClick, order};
-  },
-
--->
