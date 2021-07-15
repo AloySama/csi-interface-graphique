7317f4ef667aa15848af2ece011a260918f52cef
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-15 16:30:12 +0200

    [+] Correction d'un petit bug où le bouton retour ne fonctionnait pas [~] Modification du .css où le margin-bottom ne se fait plus avec les boutons mais les div

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,23 @@
       <div class="order">
         <button id="edit" :title="message['edit']" class="hover-item btn btn-cancel" @click="doEdit(true)">Éditer ficher ARCOLE</button>
         <button id="retour" :title="message['retour']" class="hover-item" v-if="editing" @click="doEdit(false);
-        societe = !societe; edit_societe = false">Retour</button>
+        is_societe = !is_societe; edit_societe = false">Retour</button>
       </div>
     </header>
   </div>
   <template v-if="editing">
-    <button class="hover-item" @click="societe = !societe; edit_json(true, 2)">Ajouter une société</button>
+    <button class="hover-item" @click="is_societe = !is_societe; edit_json(true, 2)">Ajouter une société</button>
     <button class="hover-item" @click="etablissement = !etablissement">Ajouter un établissement</button>
     <button class="hover-item">Ajouter une restaurant</button>
     <button v-if="edit_societe" class="hover-item" @click="edit_societe = false; edit_json(false, 2)">Retour</button>
   </template>
-  <SocieteForm v-if="societe"/>
+  <SocieteForm v-if="edit_societe"/>
   <EtablissementForm v-if="etablissement"></EtablissementForm>
   <div id="app2"></div>
   <footer> <a href="https://www.linkedin.com/in/alo%C3%AFs-brengard/" target="_blank">Author: Aloïs BRENGARD</a></footer>
 </template>
 
 <script lang="ts">
-/*// eslint-disable-next-line @typescript-eslint/no-var-requires
-const json = require('../../json_file/arcole.json');*/
 import {defineComponent} from "vue";
 import EditSociete from "@/functions/edit_json";
 import SocieteForm from "@/components/SocieteForm.vue";
@@ -36,7 +34,7 @@ export default defineComponent({
   components: {EtablissementForm, SocieteForm},
   data() {
     return {
-      societe: false,
+      is_societe: false,
       etablissement: false,
       editing: false,
       edit_societe: false,
