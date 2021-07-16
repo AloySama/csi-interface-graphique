15e5a42f043cd1512324b0c9c9669d484b56f11a
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-16 10:51:39 +0200

    [+] Ajout d'un bouton permettant de charger un fichier depuis le pc (non fonctionnel)

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,29 @@
   <div class="app">
     <header>
       <div class="title">
-        <a href="https://www.cashsystemes.eu/"><img src="../../images/csi.png" alt="logo"
+        <a href="https://www.cashsystemes.eu/"><img src="../../images/csi.png" alt="logo CSI"
                                                     title="https://www.cashsystemes.eu/"></a>
         <h1>ARCOLE<br>export</h1>
       </div>
       <div class="order">
-        <button id="edit" :title="message['edit']" class="hover-item btn btn-cancel" @click="doEdit(true)">Éditer ficher ARCOLE</button>
-        <button id="retour" :title="message['retour']" class="hover-item" v-if="editing" @click="doEdit(false);
-        is_societe = !is_societe; edit_societe = false">Retour</button>
+        <button id="edit" :title="message['edit']" class="hover-item" @click="doEdit(true, true, 0)">Éditer ficher ARCOLE</button>
+        <input id="fileUpload" type="file" hidden>
+        <button id="parcourir" :title="message['parcours']" class="hover-item" @click="ChooseFiles()">Charger un fichier</button>
+        <button id="retour" :title="message['retour']" class="hover-item" v-if="editing" @click="doEdit(true, false, 0);
+        edit_societe = false; edit_eta = false">Retour</button>
       </div>
     </header>
   </div>
   <template v-if="editing">
-    <button class="hover-item" @click="is_societe = !is_societe; edit_json(true, 2)">Ajouter une société</button>
-    <button class="hover-item" @click="etablissement = !etablissement">Ajouter un établissement</button>
+    <button class="hover-item" @click="edit_societe = !edit_societe; doEdit(false, true, 3)">Ajouter une société</button>
+    <button class="hover-item" @click="edit_eta = !edit_eta">Ajouter un établissement</button>
     <button class="hover-item">Ajouter une restaurant</button>
-    <button v-if="edit_societe" class="hover-item" @click="edit_societe = false; edit_json(false, 2)">Retour</button>
+    <button v-if="edit_societe || edit_eta" class="hover-item" @click="edit_societe = false; edit_eta = false; doEdit(false, false, 3)">Retour</button>
   </template>
   <SocieteForm v-if="edit_societe"/>
-  <EtablissementForm v-if="etablissement"></EtablissementForm>
+  <EtablissementForm v-if="edit_eta"></EtablissementForm>
   <div id="app2"></div>
+  <div class="left">{{ date }}</div>
   <footer> <a href="https://www.linkedin.com/in/alo%C3%AFs-brengard/" target="_blank">Author: Aloïs BRENGARD</a></footer>
 </template>
 
@@ -34,27 +37,44 @@ export default defineComponent({
   components: {EtablissementForm, SocieteForm},
   data() {
     return {
-      is_societe: false,
-      etablissement: false,
+      edit_eta: false,
       editing: false,
       edit_societe: false,
       message: {
         'edit': 'Cliquer sur moi pour commencer à éditer le fichier.',
-        'retour': 'Sauvegarde et quitte l\'édition'
+        'retour': 'Sauvegarde et quitte l\'édition',
+        'parcours': 'Parcourir un fichier depuis le bureau.'
       },
+      date: 'Vous avez affiché cette page le ' + new Date().toLocaleString(),
       EditSociete
     }
   },
   name: 'App',
   methods: {
-    doEdit(editing: boolean) {
-      this.editing = editing;
-      document.querySelectorAll('button')[0].disabled = editing;
-    },
-    edit_json(editing: boolean, int: number) {
-      this.edit_societe = editing;
+    doEdit(must_edit: boolean, editing: boolean, int: number) {
+      if (must_edit)
+        this.editing = editing;
       document.querySelectorAll('button')[int].disabled = editing;
+    },
+    ChooseFiles: function() {
+      let doc = document.getElementById("fileUpload");
+      if (doc == null) {
+        console.error('Une erreur est survenue.');
+        return false;
+      }
+      doc.click()
+      console.log(doc)
+      return true;
     }
   }
 });
 </script>
+
+<style scoped>
+.left {
+  position: absolute;
+  bottom: 0;
+  left: 10px;
+  font-size: 12px;
+}
+</style>
