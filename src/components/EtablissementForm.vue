3d31b19f997da9fa28b0214241e4e0711543bf65
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-19 11:10:32 +0200

    [+] Début de création du formulaire pour l'ajout d'établissements.

--- src/components/EtablissementForm.vue
+++ src/components/EtablissementForm.vue
@@ -1,12 +1,27 @@
+<!-- Afficher les différents sociétés pour que l'utilisateur choisisse puis faire le formulaire -->
+
 <template>
-  <div>
-    Nothing right here...
+  <div v-for="(soc, index) in ParseSociete(json)" :key="index">
+    <button class="hover-item" @click="FillSociete(index)">{{soc}}</button>
   </div>
 </template>
 
 <script>
+import ParseSociete from "../functions/ParseSociete";
+const FillTab = {societe: 0}
 export default {
-  name: "EtablissementForm"
+  name: "EtablissementForm",
+  data() {
+    return {
+      ParseSociete,
+      json: require('../../../json_file/test_file.json')
+    }
+  },
+  methods: {
+    FillSociete(s) {
+      FillTab['societe'] = s;
+    }
+  }
 }
 </script>
 
