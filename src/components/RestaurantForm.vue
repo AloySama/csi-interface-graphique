de52fb1ba3d7cd09d2917f78527720ca9d6c093c
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-21 12:02:36 +0200

    [~] Quelques modifications

--- src/components/RestaurantForm.vue
+++ src/components/RestaurantForm.vue
@@ -5,16 +5,16 @@
  <div>
     Choisissez à quelle société vous voulez ajouter l'établissement. <br>
     <ul>
-      <li class="OneLine" v-for="(soc, index) in ParseSociete(json)" :key="index">
-        <button :id="'ButtonEta' + index" class="hover-item" @click="DisabledButton('ButtonEta', index, true); HasChanged(index); FillSociete(index);">{{soc}}</button>
+      <li class="OneLine" v-for="(soc, index_soc) in ParseSociete(json)" :key="index_soc">
+        <button :id="'ButtonEta' + index_soc" class="hover-item" @click="DisabledButton('ButtonEta', index_soc, true); HasChanged(index_soc); FillSociete(index_soc); current = index_soc">{{soc}}</button>
       </li>
       <button class="hover-item" @click="FillSociete(-1); App.methods.doEdit(false, false, 'AddRes'); $emit('edit_value', false)">Retour</button>
     </ul>
   </div>
   <div v-if="FillTab['societe'] >= 0">
     <ul>
-      <li class="OneLine" v-for="(etab, index) in ParseRestaurant(json, FillTab['societe'])" :key="index">
-        <button class="hover-item" :id="'ButtonRes' + FillTab['societe'] + index" @click="FillEtab(index); ">{{etab}}</button>
+      <li class="OneLine" v-for="(etab, index_eta) in ParseRestaurant(json, FillTab['societe'])" :key="index_eta">
+        <button class="hover-item" :id="'ButtonRes' + FillTab['societe'] + index_eta" @click="FillEtab(index_eta);">{{etab}}</button>
       </li>
     </ul>
   </div>
@@ -55,7 +55,8 @@ export default {
       FillTab: {'societe': -1, 'etablissement': -1},
       AddMatricule: false,
       AddId: false,
-      AddTdd: false
+      AddTdd: false,
+      current: -1
     }
   },
   methods: {
@@ -66,22 +67,36 @@ export default {
       this.FillTab['etablissement'] = etablissement;
       console.log(this.FillTab);
     },
-    DisabledButton(ElementId, i,bool) {
+    DisabledButton(ElementId, i, bool) {
       document.getElementById(ElementId + i).disabled = bool;
-      for(let j = 0; j < ParseSociete(this.json).length; j++) {
+      for(let j = 0; j < this.CountSociete(); j++) {
+        if (j === i) continue;
         try {
-          if (j === i) continue;                                            //TODO: Il faudrait vérifier en même temps l'id de l'établissement ET celui de la société pour skip
-          document.getElementById(ElementId + j).disabled = false;
+          document.getElementById(ElementId + j).disabled = !bool;
         }
         catch (e) {
           return;
         }
       }
-      console.log(this.FillTab)
+    },
+    DisabledButtonRes(IdButtonClicked) { //TODO: revoir cette fonction pour désactiver les boutons
+      console.log('ButtonRes' + IdButtonClicked);
+      document.getElementById('ButtonRes' + IdButtonClicked).disabled = true;
+      for (let j  = 0; j < this.CountSociete; j++) {
+        for (let i = 0; i < this.CountRes(j, i); i++) {
+          document.getElementById('ButtonRes' + j + i).disabled = false;
+        }
+      }
     },
     HasChanged(index) {
       if (!(this.FillTab['societe'] === index))
         this.FillTab['etablissement'] = -1;
+    },
+    CountSociete() {
+      return this.json.length
+    },
+    CountRes(soc, eta) {
+      return this.json[soc].etablissements[eta].length;
     }
   }
 }
