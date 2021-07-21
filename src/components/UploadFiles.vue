bb9a57f6a13666e28324011ee92f324751e84beb
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-21 12:03:24 +0200

    [+] Il est possible d'ajouter un fichier sur son pc vers le site (pas encore fonctionnel pour éditer ...)

--- src/components/UploadFiles.vue
+++ src/components/UploadFiles.vue
@@ -1,32 +1,55 @@
 <template>
-  <form @submit.prevent="" class="form" id="MyForm">
-    <input id="fileUpload" type="file" required accept=".json" @change="OnFileSelected">
-    <!--<button class="hover-item" @click="ChooseFiles">Parcourir ...</button>-->
-    <input type="submit">
-    <!--<button class="hover-item" disabled>OK</button> -->
-  </form>
+  <div>
+    <form @submit.prevent="" class="form" id="MyForm">
+      <input name="UploadFile" id="UploadFiles" type="file" required accept=".json" @change="loadTextFromFile">
+    </form>
+  </div>
+  <div id="container" style="height: 500px; min-width: 500px"></div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      file: null
-    }
+  data: () => ({ text: "" }),
+    components: {
+      FileReader
   },
   name: "UploadFiles",
   methods: {
-    OnFileSelected(e) {
-      console.log(e);
-      this.file = e.target.files[0];
-      console.log(this.file);
+    loadTextFromFile(ev) {
+
+      const file = ev.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        document.getElementById('container').innerHTML=e.target.result;
+      }
+      reader.readAsText(file, 'utf-8');
     }
   }
 }
+
 </script>
 
-<style scoped>
 
+<style>
+.text-reader {
+  position: relative;
+  overflow: hidden;
+  display: inline-block;
+
+  /* Fancy button style 😎 */
+  border: 2px solid black;
+  border-radius: 5px;
+  padding: 8px 12px;
+  cursor: pointer;
+}
+.text-reader input {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  opacity: 0;
+}
 </style>
 
 <!--TODO: Faire communiquer le front et le back pour pouvoir modifier par la suite le json uploadé -->
