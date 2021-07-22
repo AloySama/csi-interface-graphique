466c06bee58b38fb385c4b6b17c40fb98013629f
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-22 10:39:48 +0200

    [-]  Suppression de commentaires inutiles

--- src/components/SocieteForm.vue
+++ src/components/SocieteForm.vue
@@ -79,34 +79,3 @@ export default {
   padding: 20px;
 }
 </style>
-
-
-
-
-<!--
-<style scoped>
- input[type=submit] {
-  width: 100%;
-  background-color: #a100ff;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-input[type=submit]:hover {
-  background-color: rebeccapurple;
-}
-
-input[type=text], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-</style>-->
