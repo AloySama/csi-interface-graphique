443da59ac0fa8ed9ee61bb583830f20981e8b67d
Aloïs BRENGARD <alois.brengard@epitech.eu>
2021-07-26 16:25:03 +0200

    [+] Installation de package pour ajouter des icons au site

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue';
 import './assets/style.css';
 import './assets/FormStyle.css';
 import './assets/UploadForm.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fas} from '@fortawesome/free-solid-svg-icons'
 
+library.add(fas);
 
-createApp(App).mount('#app');
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
